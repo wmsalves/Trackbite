1aa53f0de5dee253f074297a1fc72bf29350a0a2
wemer <wemersoncontas22@gmail.com>
2025-04-05 11:51:05 -0300

    feat: add ApplicationDbContext with DbSets

---
+++ Trackbite/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trackbite.Models;
+
+namespace Trackbite.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+        public DbSet<Habit> Habits => Set<Habit>();
+        public DbSet<HabitHistory> HabitHistories => Set<HabitHistory>();
+
+    }
+}
