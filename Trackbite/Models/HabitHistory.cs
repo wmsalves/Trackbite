c5603a2df1ad5c3850868ba8f66c2aa19714109e
wemer <wemersoncontas22@gmail.com>
2025-04-05 11:48:19 -0300

    feat: add Habit and HabitHistory models

---
+++ Trackbite/Models/HabitHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace Trackbite.Models
+{
+    public class HabitHistory
+    {
+        public int Id { get; set; }
+        public int HabitId { get; set; }
+        public Habit Habit { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
