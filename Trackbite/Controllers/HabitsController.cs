cf85d64706fc01f7b09ba884b01c3ac36e1bebd9
wemer <wemersoncontas22@gmail.com>
2025-04-07 10:09:08 -0300

    add empty HabitsController

---
+++ Trackbite/Controllers/HabitsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trackbite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HabitsController : ControllerBase
+    {
+    }
+}
