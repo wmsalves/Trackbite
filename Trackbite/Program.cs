6c4a78181abb60c88311308c94aa75526e183796
wemer <wemersoncontas22@gmail.com>
2025-04-11 11:09:31 -0300

    fix: add CommonModule to dashboard component and enable CORS in backend

--- Trackbite/Program.cs
+++ Trackbite/Program.cs
@@ -11,6 +11,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
--- front-end/src/app/components/dashboard/dashboard.component.ts
+++ front-end/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HabitService } from '../../services/habit.service';
 
 @Component({
