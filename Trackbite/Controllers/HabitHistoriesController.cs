e61308d4a89765301884e6d4fe0253954b88fd1f
wemer <wemersoncontas22@gmail.com>
2025-04-11 08:26:18 -0300

    create HabitHistoriesController structure

---
+++ Trackbite/Controllers/HabitHistoriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Trackbite.Data;
+
+namespace Trackbite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HabitHistoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public HabitHistoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
