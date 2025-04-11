8101697e7b58f7adeddbbc05ea414c28c5fd5991
wemer <wemersoncontas22@gmail.com>
2025-04-11 08:28:15 -0300

    add POST and GET by ID for HabitHistory

--- Trackbite/Controllers/HabitHistoriesController.cs
+++ Trackbite/Controllers/HabitHistoriesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Trackbite.Data;
+using Trackbite.Models;
 
 namespace Trackbite.Controllers
 {
@@ -12,6 +14,37 @@ namespace Trackbite.Controllers
         public HabitHistoriesController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        // POST: api/habithistories
+        [HttpPost]
+        public async Task<ActionResult<HabitHistory>> CreateHabitHistory(HabitHistory habitHistory)
+        {
+            // Verifica se o hábito associado existe
+            var habitExists = await _context.Habits.AnyAsync(h => h.Id == habitHistory.HabitId);
+            if (!habitExists)
+            {
+                return BadRequest("Hábito associado não encontrado.");
+            }
+
+            _context.HabitHistories.Add(habitHistory);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetHabitHistoryById), new { id = habitHistory.Id }, habitHistory);
+        }
+
+        // GET: api/habithistories/{id}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<HabitHistory>> GetHabitHistoryById(int id)
+        {
+            var habitHistory = await _context.HabitHistories.FindAsync(id);
+
+            if (habitHistory == null)
+            {
+                return NotFound();
+            }
+
+            return habitHistory;
         }
     }
 }
